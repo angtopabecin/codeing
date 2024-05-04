5897d7f9830d751e1a997769404885467770a2a8
angtopabecin <168766602+angtopabecin@users.noreply.github.com>
2024-05-04 21:18:57 +0300

    Add files via upload

---
+++ oyun.js
@@ -0,0 +1,70 @@
+const cnv = document.getElementById("harita");
+const ctx = cnv.getContext("2d");
+
+const cnvyukseklik = 800;
+const cnvgenislik = 600;
+var xEksen = 0;
+
+const draw = (x,y,w,h,renk) =>{
+    ctx.fillStyle = renk;
+    ctx.fillRect(x,y,w,h);
+}
+
+
+const draw2 = (x,y,w,h,renk) =>{
+    ctx.fillStyle = renk;
+    ctx.fillRect(x,y,w,h);
+    
+}
+
+const karakter = {
+    x:40,
+    y:20,
+    w:40,
+    h:40,
+    renk:'black',
+    xHareket:2,
+    yYareket:3,
+}
+
+const ciz = () => {
+    draw(0,0,cnvyukseklik,cnvgenislik,"orange");
+    draw2(karakter.x,karakter.y,karakter.w,karakter.h,karakter.renk);
+}
+
+
+
+
+const game = () => {
+    ciz();
+    
+}
+
+const kare  = 50;
+setInterval(game ,1000/kare);
+
+
+
+
+const hareket = () => {
+    karakter.x = 50;
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
